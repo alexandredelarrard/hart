9ff080241cd79a457c062e05d5e0b05061900514
alexandredelarrard <a.larrard@gmail.com>
2024-05-28 17:27:03 +0200

    nginx & gunicorn for backend api

--- backend-api/app/chroma_db.py
+++
@@ -1,9 +0,0 @@
-def compare_embeddings(embedding):
-    # Dummy function to compare embeddings
-    # Assume we have a function `get_top_50_similar_embeddings` that interacts with Chroma DB
-    results = get_top_50_similar_embeddings(embedding)
-    return results
-
-def get_top_50_similar_embeddings(embedding):
-    # Dummy data for similar embeddings
-    return [{"id": i, "similarity": 0.99 - i*0.01} for i in range(50)]
