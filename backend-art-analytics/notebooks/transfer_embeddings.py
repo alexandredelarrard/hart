cd5630773c226078a339fc90c66d4266ef60f371
alexandredelarrard <a.larrard@gmail.com>
2024-06-25 14:15:26 +0200

    testing embeddings working on sample

--- backend-art-analytics/notebooks/transfer_embeddings.py
+++ backend-art-analytics/notebooks/transfer_embeddings.py
@@ -14,12 +14,12 @@ chroma = ChromaCollection(context=context,
                         n_top_results=25)
 
 ids = chroma.collection.get(include=[])["ids"]
-step = 30000
-with open(r'D:\db_backup\picture_embeddings.csv', 'a', newline='') as csvfile:
+step = 100
+with open(r'D:\db_backup\picture_embeddings_truncated.csv', 'a', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['ID_UNIQUE', 'ID_PICTURE', "pict_path", 'CREATED_AT', 'EMBEDDING'])
     
-    for i in tqdm.tqdm(range(len(ids)// step)):
+    for i in tqdm.tqdm(range(1)):
         sub_ids = ids[i*step:min((i+1)*step, len(ids))]
         all_data = chroma.collection.get(ids=sub_ids, include=["embeddings", "metadatas"])
         for id, embdding, metadata in zip(all_data["ids"], all_data["embeddings"], all_data["metadatas"]):
